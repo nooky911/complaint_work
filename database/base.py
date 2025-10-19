00a70b2673e3b0b60fc6ce63f0d55b48dbdcf493
nooky911 <il.pozdin2012@yandex.ru>
2025-10-19 17:49:36 +0500

    fix:Изменил управление открытия и закрытия сессий

--- database/base.py
+++ database/base.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from config import settings
 
+
 DATABASE_URL = settings.get_db_url()
 
 engine = create_async_engine(url=DATABASE_URL)
@@ -11,10 +12,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False, echo=Tr
 class Base(DeclarativeBase):
     pass
 
-# Декоратор для автоматического открытия и закрытия сессий
-def db_session(method):
-    async def wrapper(*args, **kwargs):
-        async with async_session_maker() as session:
-            async with session.begin():
-                return await method(*args, session=session, **kwargs)
-    return wrapper
+async def get_db():
+    async with async_session_maker() as session:
+        async with session.begin():
+            yield session
