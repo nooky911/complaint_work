e68daf62ba5f3f6f400b42e0e25a90c729b2145e
nooky911 <pozdin.99@mail.ru>
2025-10-27 14:45:31 +0500

    Добавлен импорт схем из filters и references

--- myapp/schemas/__init__.py
+++ myapp/schemas/__init__.py
@@ -1,11 +1,24 @@
 from .cases import CaseCreate, CaseUpdate, CaseList, CaseDetail
 from .warranty import WarrantyWorkUpdate, WarrantyWorkResponse
+from .filters import FilterOption, FilterOptionsResponse, CaseFilterParams
+from .references import AuxiliaryItem
 
 __all__ = [
+    # Схемы для случаев
     "CaseCreate",
     "CaseUpdate",
     "CaseList",
     "CaseDetail",
+
+    # Схемы для Рекламационной работы
     "WarrantyWorkUpdate",
-    "WarrantyWorkResponse"
+    "WarrantyWorkResponse",
+
+    # Схемы для Фильтров
+    "FilterOption",
+    "FilterOptionsResponse",
+    "CaseFilterParams",
+
+    # Базовая схема для таблиц формата id, name
+    "AuxiliaryItem"
 ]
