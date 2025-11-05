38765c96cf1cf5347f2940226188b9566afa9637
nooky911 <pozdin.99@mail.ru>
2025-11-05 09:55:27 +0500

    Добавил обработчик ошибок в роут по обновлению данных случая

--- myapp/api/v1/endpoints/case_routes.py
+++ myapp/api/v1/endpoints/case_routes.py
@@ -5,71 +5,110 @@ from myapp.schemas.cases import CaseList, CaseDetail, CaseCreate, CaseUpdate
 from myapp.schemas.filters import CaseFilterParams
 from myapp.services.case_service import CaseService
 from myapp.services.case_filter_service import CaseFilterService
+from myapp.services.warranty_service import WarrantyService
 from myapp.database.base import get_db
 from .warranty_routes import router as warranty_router
 
 
-router = APIRouter(prefix="/cases", tags=["Случаи неисправности"] )
+router = APIRouter(prefix="/cases", tags=["Случаи неисправности"])
 
-router.include_router(warranty_router)
+router.include_router(warranty_router, prefix="/{case_id}/warranty")
 
 
 # Полный список и фильтрация
 @router.get(
-    "/", response_model=list[CaseList],
+    "/",
+    response_model=list[CaseList],
     summary="Получить список случаев ремонта с (возможной) фильтрацией",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
-async def list_and_filter_cases(params: CaseFilterParams = Depends(), session: AsyncSession = Depends(get_db)):
+async def list_and_filter_cases(
+    params: CaseFilterParams = Depends(), session: AsyncSession = Depends(get_db)
+):
+    """Получить список случаев неисправности, используя параметры фильтрации"""
     return await CaseFilterService.filter_cases(session, params)
 
 
 # Создание
-@router.post("/", response_model=CaseDetail, status_code=status.HTTP_201_CREATED, summary="Создание случая")
+@router.post(
+    "/",
+    response_model=CaseDetail,
+    status_code=status.HTTP_201_CREATED,
+    summary="Создание случая",
+)
 async def create_case(case_data: CaseCreate, session: AsyncSession = Depends(get_db)):
+    """Создает новый случай неисправности и автоматически создает связанную запись WarrantyWork"""
     return await CaseService.create_case(session, case_data)
 
 
 # Детали случаи
-@router.get("/{case_id}", response_model=CaseDetail, summary="Получить детальную информацию о случае")
+@router.get(
+    "/{case_id}",
+    response_model=CaseDetail,
+    summary="Получить детальную информацию о случае",
+)
 async def get_case_detail(
-        case_id: int = Path(..., description="ID случая неисправности", ge=1),
-        session: AsyncSession = Depends(get_db)
+    case_id: int = Path(..., description="ID случая неисправности", ge=1),
+    session: AsyncSession = Depends(get_db),
 ):
+    """Получает полную информацию о случае по его ID, включая все связанные данные"""
     case = await CaseService.get_case(session, case_id)
     if not case:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Случай с ID {case_id} не найден")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Случай с ID {case_id} не найден.",
+        )
     return case
 
 
 # Редактирование
 @router.patch("/{case_id}", response_model=CaseDetail, summary="Редактирование случая")
 async def update_case(
-        case_data: CaseUpdate,
-        case_id: int = Path(..., description="ID случая неисправности", ge=1),
-        session: AsyncSession = Depends(get_db)
+    case_data: CaseUpdate,
+    case_id: int = Path(..., description="ID случая неисправности", ge=1),
+    session: AsyncSession = Depends(get_db),
 ):
+    """Обновляет основные поля случая"""
     updated_case = await CaseService.update_case(session, case_id, case_data)
+
     if not updated_case:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Случай с ID {case_id} для обновления не найден"
+            detail=f"Случай с ID {case_id} для обновления не найден.",
         )
-    return updated_case
+
+    # Обновление данных по рекл. работе
+    if case_data.warranty_work:
+        await WarrantyService.update_warranty_work(
+            session, case_id, case_data.warranty_work
+        )
+
+    final_case = await CaseService.get_case(session, case_id)
+
+    if not final_case:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Случай с ID {case_id} не найден после обновления.",
+        )
+
+    return final_case
 
 
-#Удаление случая
-@router.delete("/{case_id}", status_code=status.HTTP_204_NO_CONTENT, summary="Удалить случай по ID")
+# Удаление случая
+@router.delete(
+    "/{case_id}", status_code=status.HTTP_204_NO_CONTENT, summary="Удалить случай по ID"
+)
 async def delete_case(
-        case_id: int = Path(..., description="ID случая неисправности", ge=1),
-        session: AsyncSession = Depends(get_db)
+    case_id: int = Path(..., description="ID случая неисправности", ge=1),
+    session: AsyncSession = Depends(get_db),
 ):
+    """Удаляет случай по его ID. Также удаляет связанные записи WarrantyWork"""
     deleted_count = await CaseService.delete_case(session, case_id)
 
     if deleted_count == 0:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Случай с ID {case_id} не найден."
+            detail=f"Случай с ID {case_id} не найден.",
         )
 
     return None
