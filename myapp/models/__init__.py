8ac954c67b1a84085abe0b6698c5e5880e14edc7
nooky911 <pozdin.99@mail.ru>
2025-12-17 13:44:11 +0500

    Добавил CRUD операции по файлам.

--- myapp/models/__init__.py
+++ myapp/models/__init__.py
@@ -1,6 +1,11 @@
 from .warranty_work import WarrantyWork
 from .equipment_malfunctions import Equipment, EquipmentMalfunction, Malfunction
 from .repair_case_equipment import RepairCaseEquipment
+from .case_files import (
+    CaseFile,
+    FileCategory,
+    WarrantyDocumentField,
+)
 from .auxiliaries import (
     RegionalCenter,
     LocomotiveModel,
