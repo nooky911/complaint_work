cd1b41144a332755b60f5c2547de552bb30fc771
nooky911 <pozdin.99@mail.ru>
2025-12-29 14:01:20 +0500

    Добавил url фронта

--- myapp/main.py
+++ myapp/main.py
@@ -68,7 +68,7 @@ async def log_requests(request: Request, call_next):
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ПОМЕНЯТЬ
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
