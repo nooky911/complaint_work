69c419f443f364ae7d31cc9d2204aec487358168
nooky911 <pozdin.99@mail.ru>
2026-01-20 15:21:05 +0500

    Улучшил фильтрацию и добавил возможность автоматического заполнения оборудования с любого уровня

--- myapp/schemas/__init__.py
+++ myapp/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .cases import CaseCreate, CaseUpdate, CaseList, CaseDetail
 from .warranty import WarrantyWorkUpdate, WarrantyWorkResponse
-from .filters import FilterOption, FilterOptionsResponse, CaseFilterParams
+from .filters import FilterOptionsResponse, CaseFilterParams
 from .references import AuxiliaryItem
 
 __all__ = [
@@ -9,16 +9,12 @@ __all__ = [
     "CaseUpdate",
     "CaseList",
     "CaseDetail",
-
     # Схемы для Рекламационной работы
     "WarrantyWorkUpdate",
     "WarrantyWorkResponse",
-
     # Схемы для Фильтров
-    "FilterOption",
     "FilterOptionsResponse",
     "CaseFilterParams",
-
     # Базовая схема для таблиц формата id, name
-    "AuxiliaryItem"
+    "AuxiliaryItem",
 ]
