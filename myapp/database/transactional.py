037411127c476c1aeeebd8890bfd885f677b0dce
nooky911 <pozdin.99@mail.ru>
2026-02-26 16:42:44 +0500

    Улучшил декоратор сессии. Теперь нет строгой привязке к аргументам функции

--- myapp/database/transactional.py
+++ myapp/database/transactional.py
@@ -1,25 +1,32 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from functools import wraps
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def transactional(func):
-    """Декоратор, который управляет транзакцией для асинхронного сервисного метода"""
+    """Декоратор управления транзакцией"""
 
     @wraps(func)
     async def wrapper(*args, **kwargs):
-        if args and isinstance(args[0], AsyncSession):
-            session: AsyncSession = args[0]
-        else:
-            raise RuntimeError("Неверный первый аргумент")
+        session = kwargs.get("session")
+        if not session:
+            for arg in args:
+                if isinstance(arg, AsyncSession):
+                    session = arg
+                    break
+
+        if not session or not isinstance(session, AsyncSession):
+            raise RuntimeError(f"Неверный аргумент сессии в {func.__name__}")
 
         try:
             result = await func(*args, **kwargs)
-
             await session.commit()
             return result
-
         except Exception as e:
             await session.rollback()
-            raise
+            logger.error(f"{func.__name__}: {e}")
+            raise e
 
     return wrapper
