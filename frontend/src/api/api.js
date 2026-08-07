cdec35d7c223bb4543bdb92af3691ebb4de780a4
Ilya <pozdin.99@mail.ru>
2026-08-07 13:18:31 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? "/api/v1" : "http://localhost:8000/api/v1",
+  baseURL: "/api/v1",
   withCredentials: true,
 });
 
